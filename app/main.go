a1aaed5c6ebc60f63369bdb104b25a242296bfef
theoneLee <623507020@qq.com>
2019-11-26 15:37:04 +0800

    improve the usage

--- app/main.go
+++ app/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"core_gin/app/core"
+	"encoding/json"
 	"fmt"
 )
 
@@ -25,5 +26,29 @@ func main() {
 		//	panic(err)
 		//}
 	})
+
+	v1 := router.Group("/v1")
+	v1.Use(middleWareHandler)
+	v1.POST("/post", func(ctx *core.Context) {
+		pm := ctx.Request.URL.Query()
+		if v, ok := pm["id"]; ok {
+			fmt.Println("request url", ctx.Request.URL.String(), " parameter id value =", v)
+		}
+		data := map[string]interface{}{}
+		data["data"] = fmt.Sprintf("post id is %v", pm["id"][0])
+		str, err := json.Marshal(data)
+		if err != nil {
+			panic(err)
+		}
+		ctx.ResponseWriter.Header().Add("Content-Type", "application/json")
+		//ctx.ResponseWriter.WriteHeader(200)
+		ctx.ResponseWriter.Write(str)
+	})
+
 	router.Run(":2222")
+}
+
+func middleWareHandler(ctx *core.Context) {
+	//todo ctx.Abort() and ctx.Next()
+	fmt.Println("exec the middleware ")
 }
